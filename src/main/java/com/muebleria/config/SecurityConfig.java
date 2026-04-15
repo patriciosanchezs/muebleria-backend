5a9cb51cbffa36828db13daadc7050051d360e0e
Patricio Sanchez <psanchezs@forus.cl>
2026-04-15 09:02:57 -0400

    add front url cors

--- src/main/java/com/muebleria/config/SecurityConfig.java
+++ src/main/java/com/muebleria/config/SecurityConfig.java
@@ -67,7 +67,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"
+                , "http://localhost:4200"
+                , "https://forniture-store-cl.netlify.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
